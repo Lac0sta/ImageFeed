318bd7b9b70f8f739fa7f2e2614ead9281143923
Lac0sta <adv.a.frolov@gmail.com>
2025-11-14 16:14:49 +0100

    Initial commit, add .gitignore

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Aleksei Frolov on 14. 11. 2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
